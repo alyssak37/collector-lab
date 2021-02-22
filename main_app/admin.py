d87c7be1a13b6094961688093b299ed60d08a937
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-02-22 10:53:00 -0500

    adds toppings to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Icecream, Eating
+from .models import Icecream, Eating, Topping
 # Register your models here.
 
 admin.site.register(Icecream)
 admin.site.register(Eating)
+admin.site.register(Topping)
